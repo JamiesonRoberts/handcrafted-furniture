8605020e9e052beac1190fa2742b68fa70db50f8
Jamieson Roberts <hello@jamiesonroberts.com>
2023-01-15 12:39:26 -0500

    Logo rebuild, favicon, sitemap.xml and robots.txt

--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,26 @@
-import PlausibleProvider from "next-plausible";
+import PlausibleProvider from 'next-plausible'
+import { EB_Garamond, Oswald } from '@next/font/google'
+
+const garamond = EB_Garamond({ subsets: ['latin'] })
+const oswald = Oswald({ subsets: ['latin'] })
+
 import '../styles/globals.css'
 
-const App = ({Component, pageProps}) => {
+const App = ({ Component, pageProps }) => {
     return (
-        <PlausibleProvider domain='handcraftedfurniture.ca'
-                           trackOutboundLinks={true}>
+        <PlausibleProvider
+            domain='handcraftedfurniture.ca'
+            trackOutboundLinks={true}
+        >
+            <style jsx global>{`
+                :root {
+                    --garamond: ${garamond.style.fontFamily};
+                    --oswald: ${oswald.style.fontFamily};
+                }
+            `}</style>
             <Component {...pageProps} />
         </PlausibleProvider>
     )
 }
 
-export default App;
+export default App
