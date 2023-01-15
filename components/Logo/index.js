cb8e6bb6270c9997e26dbc8ce8ead9905170733a
Jamieson Roberts <hello@jamiesonroberts.com>
2023-01-15 12:39:26 -0500

    Logo rebuild, favicon, sitemap.xml and robots.txt

--- components/Logo/index.js
+++ components/Logo/index.js
@@ -1,14 +1,20 @@
-import classnames from 'classnames';
-import { EB_Garamond } from '@next/font/google'
 import styles from './index.module.css'
+import Image from 'next/image'
 
-const garamond = EB_Garamond({ subsets: ['latin'] })
+import mapleLeaf from '../../public/maple-leaf.png'
 
 export default function Logo() {
     return (
-        <span className={classnames(garamond.className, styles.mainText)}>
+        <span className={styles.mainText}>
             Handcrafted Furniture
             <span className={styles.subText}>By Ken Roberts</span>
+            <Image
+                priority
+                src={mapleLeaf}
+                className={styles.icon}
+                width={38}
+                height={41}
+            />
         </span>
     )
 }
--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,67 @@
 import Head from 'next/head'
-import Logo from "../components/Logo";
+import Logo from '../components/Logo'
 
 export default function Home() {
     return (
         <>
             <Head>
+                <meta
+                    name='viewport'
+                    content='width=device-width, initial-scale=1'
+                />
+
                 <title>Handcrafted Furniture by Ken Roberts</title>
-                <meta name="description"
-                      content="Handcrafted Furniture by Ken Roberts. Handcrafted and Custom designed wooden Furniture for interior and garden. Designed and built for your home decor."/>
-                <meta name="viewport" content="width=device-width, initial-scale=1"/>
-                <link rel="icon" href="/favicon.ico"/>
+                <meta
+                    name='description'
+                    content='Handcrafted Furniture by Ken Roberts. Handcrafted and Custom designed wooden Furniture for interior and garden. Designed and built for your home decor.'
+                />
+
+                <link
+                    rel='icon'
+                    href='https://handcraftedfurniture.ca/favicon.ico'
+                />
+                <link
+                    rel='apple-touch-icon'
+                    sizes='180x180'
+                    href='https://handcraftedfurniture.ca/apple-touch-icon.png'
+                />
+                <link
+                    rel='icon'
+                    type='image/png'
+                    sizes='32x32'
+                    href='https://handcraftedfurniture.ca/favicon-32x32.png'
+                />
+                <link
+                    rel='icon'
+                    type='image/png'
+                    sizes='16x16'
+                    href='https://handcraftedfurniture.ca/favicon-16x16.png'
+                />
+                <link
+                    rel='manifest'
+                    href='https://handcraftedfurniture.ca/site.webmanifest'
+                />
+                <link
+                    rel='mask-icon'
+                    href='https://handcraftedfurniture.ca/safari-pinned-tab.svg'
+                    color='#1c1c1b'
+                />
+                <meta name='msapplication-TileColor' content='#1c1c1b' />
+                <meta
+                    name='theme-color'
+                    content='#F7F7F7'
+                    media='(prefers-color-scheme: light)'
+                />
+                <meta
+                    name='theme-color'
+                    content='#1C1C1B'
+                    media='(prefers-color-scheme: dark)'
+                />
             </Head>
             <main>
-                <h1><Logo/></h1>
+                <h1>
+                    <Logo />
+                </h1>
             </main>
         </>
     )
