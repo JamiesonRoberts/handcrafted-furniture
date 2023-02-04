d9f845a472ab813e3717cc0583a6f7893daaf3d3
Jamieson Roberts <hello@jamiesonroberts.com>
2023-02-04 15:16:59 -0500

    Working on final part and starting into SEO things

--- components/Aside/index.js
+++ components/Aside/index.js
@@ -3,8 +3,9 @@ import classNames from 'classnames'
 import styles from './index.module.css'
 export default function Aside({ className, ...rest }) {
     return (
-        <aside className={classNames(styles.aside, className)} {...rest}>
-            <p>&copy;2023 Handcrafted Furniture by Ken Roberts</p>
-        </aside>
+        <aside
+            className={classNames(styles.aside, className)}
+            {...rest}
+        ></aside>
     )
 }
