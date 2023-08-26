5084072a18c5247a7e7a06134cb58575959846bf
Jamieson Roberts <hello@jamiesonroberts.com>
2023-08-26 14:22:47 -0400

    Working on LCP busting

--- components/Aside/index.js
+++ components/Aside/index.js
@@ -5,71 +5,73 @@ import classNames from 'classnames'
 import styles from './index.module.css'
 import { useRef, useState } from 'react'
 
+import stakeTruck from '../../public/gallery/stake-truck.jpg'
+
 const galleryContent = [
     {
         img: {
-            src: '/gallery/stake-truck.jpg',
+            src: stakeTruck,
             ratio: 768 / 1024,
         },
         caption: 'Stake Truck of various hardwoods',
     },
-    {
-        img: {
-            src: '/gallery/red-oak-buffet-hutch.jpg',
-            ratio: 730 / 954,
-        },
-        caption: 'Red Oak Buffet & Hutch',
-    },
-    {
-        img: {
-            src: '/gallery/lutyen-bench-v2.jpg',
-            ratio: 1350 / 1080,
-        },
-        caption: 'Lutyen Bench in White Oak',
-    },
-    {
-        img: {
-            src: '/gallery/chair-step-ladder.jpg',
-            ratio: 1200 / 757,
-        },
-        caption: 'Chair / Step Ladder in Oak',
-    },
-    {
-        img: {
-            src: '/gallery/serving-tray-cutting-board.jpg',
-            ratio: 768 / 1024,
-        },
-        caption:
-            'Serving Tray & Cutting Board - Hard Maple / Black Walnut Butterfly Joints',
-    },
-    {
-        img: {
-            src: '/gallery/patio-table.jpg',
-            ratio: 677 / 994,
-        },
-        caption: 'White Oak Patio Table with Extension Leaf',
-    },
-    {
-        img: {
-            src: '/gallery/cherry-night-table.jpg',
-            ratio: 1024 / 768,
-        },
-        caption: 'Cherry Night Table with Aromatic Cedar Drawer & Interior',
-    },
-    {
-        img: {
-            src: '/gallery/bent-wood-music-stand.jpg',
-            ratio: 1006 / 500,
-        },
-        caption: 'Bent Wood Music Stand in White Oak',
-    },
-    {
-        img: {
-            src: '/gallery/chest-of-drawers.jpg',
-            ratio: 388 / 550,
-        },
-        caption: 'Contemporary Chest of Drawers in Ash with Onyx stain',
-    },
+    // {
+    //     img: {
+    //         src: '/gallery/red-oak-buffet-hutch.jpg',
+    //         ratio: 730 / 954,
+    //     },
+    //     caption: 'Red Oak Buffet & Hutch',
+    // },
+    // {
+    //     img: {
+    //         src: '/gallery/lutyen-bench-v2.jpg',
+    //         ratio: 1350 / 1080,
+    //     },
+    //     caption: 'Lutyen Bench in White Oak',
+    // },
+    // {
+    //     img: {
+    //         src: '/gallery/chair-step-ladder.jpg',
+    //         ratio: 1200 / 757,
+    //     },
+    //     caption: 'Chair / Step Ladder in Oak',
+    // },
+    // {
+    //     img: {
+    //         src: '/gallery/serving-tray-cutting-board.jpg',
+    //         ratio: 768 / 1024,
+    //     },
+    //     caption:
+    //         'Serving Tray & Cutting Board - Hard Maple / Black Walnut Butterfly Joints',
+    // },
+    // {
+    //     img: {
+    //         src: '/gallery/patio-table.jpg',
+    //         ratio: 677 / 994,
+    //     },
+    //     caption: 'White Oak Patio Table with Extension Leaf',
+    // },
+    // {
+    //     img: {
+    //         src: '/gallery/cherry-night-table.jpg',
+    //         ratio: 1024 / 768,
+    //     },
+    //     caption: 'Cherry Night Table with Aromatic Cedar Drawer & Interior',
+    // },
+    // {
+    //     img: {
+    //         src: '/gallery/bent-wood-music-stand.jpg',
+    //         ratio: 1006 / 500,
+    //     },
+    //     caption: 'Bent Wood Music Stand in White Oak',
+    // },
+    // {
+    //     img: {
+    //         src: '/gallery/chest-of-drawers.jpg',
+    //         ratio: 388 / 550,
+    //     },
+    //     caption: 'Contemporary Chest of Drawers in Ash with Onyx stain',
+    // },
 ]
 
 export default function Aside({ className, ...rest }) {
@@ -103,6 +105,7 @@ export default function Aside({ className, ...rest }) {
                                 <Image
                                     className={styles.image}
                                     src={content.img.src}
+                                    placeholder={'blur'}
                                     width={600}
                                     height={600 * content.img.ratio}
                                     priority={i === 0}
